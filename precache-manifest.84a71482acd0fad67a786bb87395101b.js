784828a8b1d40e32d7509f0e04803c1460885151
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-04-19 15:25:17 -0400

    Updates

---
+++ precache-manifest.84a71482acd0fad67a786bb87395101b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c9062034e47338e958e",
+    "url": "/static/css/main.89147f31.chunk.css"
+  },
+  {
+    "revision": "5c9062034e47338e958e",
+    "url": "/static/js/main.4ecdb195.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "927e02d6b05f5aadabdb76890624585f",
+    "url": "/index.html"
+  }
+];
